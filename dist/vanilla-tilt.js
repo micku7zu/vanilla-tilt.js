d6062bb61b0b090706964f33ded0ff0f2d47c970
Sebastian Limbach <bastilimbach@gmail.com>
2017-02-23 21:25:25 +0100

    New build

--- dist/vanilla-tilt.js
+++ dist/vanilla-tilt.js
@@ -1,5 +1,10 @@
-var VanillaTilt = (function () {
 'use strict';
+
+var classCallCheck = function (instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+};
 
 /**
  * Created by Șandor Sergiu (micku7zu) on 1/27/2017.
@@ -8,10 +13,13 @@ var VanillaTilt = (function () {
  * Version 1.2.0
  */
 
-class VanillaTilt {
-  constructor(element, settings = {}) {
+var VanillaTilt = function () {
+  function VanillaTilt(element) {
+    var settings = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+    classCallCheck(this, VanillaTilt);
+
     if (!(element instanceof Node)) {
-      throw ("Can't initialize VanillaTilt because " + element + " is not a Node.");
+      throw "Can't initialize VanillaTilt because " + element + " is not a Node.";
     }
 
     this.width = null;
@@ -31,7 +39,7 @@ class VanillaTilt {
     this.addEventListeners();
   }
 
-  addEventListeners() {
+  VanillaTilt.prototype.addEventListeners = function addEventListeners() {
     this.onMouseEnterBind = this.onMouseEnter.bind(this);
     this.onMouseMoveBind = this.onMouseMove.bind(this);
     this.onMouseLeaveBind = this.onMouseLeave.bind(this);
@@ -39,68 +47,67 @@ class VanillaTilt {
     this.element.addEventListener("mouseenter", this.onMouseEnterBind);
     this.element.addEventListener("mousemove", this.onMouseMoveBind);
     this.element.addEventListener("mouseleave", this.onMouseLeaveBind);
-  }
+  };
 
-  removeEventListeners() {
+  VanillaTilt.prototype.removeEventListeners = function removeEventListeners() {
     this.element.removeEventListener("mouseenter", this.onMouseEnterBind);
     this.element.removeEventListener("mousemove", this.onMouseMoveBind);
     this.element.removeEventListener("mouseleave", this.onMouseLeaveBind);
-  }
+  };
 
-  destroy() {
+  VanillaTilt.prototype.destroy = function destroy() {
     this.removeEventListeners();
     this.element.vanillaTilt = null;
     delete this.element.vanillaTilt;
 
     this.element = null;
-  }
+  };
 
-  onMouseEnter(event) {
+  VanillaTilt.prototype.onMouseEnter = function onMouseEnter(event) {
     this.updateElementPosition();
     this.element.style.willChange = "transform";
     this.setTransition();
-  }
+  };
 
-  onMouseMove(event) {
+  VanillaTilt.prototype.onMouseMove = function onMouseMove(event) {
     if (this.updateCall !== null) {
       cancelAnimationFrame(this.updateCall);
     }
 
     this.event = event;
     this.updateCall = requestAnimationFrame(this.updateBind);
-  }
+  };
 
-  onMouseLeave(event) {
+  VanillaTilt.prototype.onMouseLeave = function onMouseLeave(event) {
     this.setTransition();
 
     if (this.settings.reset) {
       this.reset();
     }
-  }
+  };
 
-  reset() {
-    requestAnimationFrame(() => {
-      this.event = {
-        pageX: this.left + this.width / 2,
-        pageY: this.top + this.height / 2
+  VanillaTilt.prototype.reset = function reset() {
+    var _this = this;
+
+    requestAnimationFrame(function () {
+      _this.event = {
+        pageX: _this.left + _this.width / 2,
+        pageY: _this.top + _this.height / 2
       };
 
-      this.element.style.transform = "perspective(" + this.settings.perspective + "px) " +
-        "rotateX(0deg) " +
-        "rotateY(0deg) " +
-        "scale3d(1, 1, 1)";
+      _this.element.style.transform = "perspective(" + _this.settings.perspective + "px) " + "rotateX(0deg) " + "rotateY(0deg) " + "scale3d(1, 1, 1)";
     });
-  }
+  };
 
-  getValues() {
-    let x = (this.event.clientX - this.left) / this.width;
-    let y = (this.event.clientY - this.top) / this.height;
+  VanillaTilt.prototype.getValues = function getValues() {
+    var x = (this.event.clientX - this.left) / this.width;
+    var y = (this.event.clientY - this.top) / this.height;
 
     x = Math.min(Math.max(x, 0), 1);
     y = Math.min(Math.max(y, 0), 1);
 
-    let tiltX = (this.reverse * (this.settings.max / 2 - x * this.settings.max)).toFixed(2);
-    let tiltY = (this.reverse * (y * this.settings.max - this.settings.max / 2)).toFixed(2);
+    var tiltX = (this.reverse * (this.settings.max / 2 - x * this.settings.max)).toFixed(2);
+    var tiltY = (this.reverse * (y * this.settings.max - this.settings.max / 2)).toFixed(2);
 
     return {
       tiltX: tiltX,
@@ -108,41 +115,41 @@ class VanillaTilt {
       percentageX: x * 100,
       percentageY: y * 100
     };
-  }
+  };
 
-  updateElementPosition() {
-    let rect = this.element.getBoundingClientRect();
+  VanillaTilt.prototype.updateElementPosition = function updateElementPosition() {
+    var rect = this.element.getBoundingClientRect();
 
     this.width = this.element.offsetWidth;
     this.height = this.element.offsetHeight;
     this.left = rect.left;
     this.top = rect.top;
-  }
+  };
 
-  update() {
-    let values = this.getValues();
+  VanillaTilt.prototype.update = function update() {
+    var values = this.getValues();
 
-    this.element.style.transform = "perspective(" + this.settings.perspective + "px) " +
-      "rotateX(" + (this.settings.axis === "x" ? 0 : values.tiltY) + "deg) " +
-      "rotateY(" + (this.settings.axis === "y" ? 0 : values.tiltX) + "deg) " +
-      "scale3d(" + this.settings.scale + ", " + this.settings.scale + ", " + this.settings.scale + ")";
-
+    this.element.style.transform = "perspective(" + this.settings.perspective + "px) " + "rotateX(" + (this.settings.axis === "x" ? 0 : values.tiltY) + "deg) " + "rotateY(" + (this.settings.axis === "y" ? 0 : values.tiltX) + "deg) " + "scale3d(" + this.settings.scale + ", " + this.settings.scale + ", " + this.settings.scale + ")";
 
     this.element.dispatchEvent(new CustomEvent("tiltChange", {
       "detail": values
     }));
 
     this.updateCall = null;
-  }
+  };
 
-  setTransition() {
+  VanillaTilt.prototype.setTransition = function setTransition() {
+    var _this2 = this;
+
     clearTimeout(this.transitionTimeout);
     this.element.style.transition = this.settings.speed + "ms " + this.settings.easing;
-    this.transitionTimeout = setTimeout(() => this.element.style.transition = "", this.settings.speed);
-  }
+    this.transitionTimeout = setTimeout(function () {
+      return _this2.element.style.transition = "";
+    }, this.settings.speed);
+  };
 
-  extendSettings(settings) {
-    let defaultSettings = {
+  VanillaTilt.prototype.extendSettings = function extendSettings(settings) {
+    var defaultSettings = {
       reverse: false,
       max: 35,
       perspective: 1000,
@@ -154,28 +161,27 @@ class VanillaTilt {
       reset: true
     };
 
-    let newSettings = {};
+    var newSettings = {};
 
     for (var property in defaultSettings) {
       if (property in settings) {
         newSettings[property] = settings[property];
       } else if (this.element.hasAttribute("data-tilt-" + property)) {
-        let attribute = this.element.getAttribute("data-tilt-" + property);
+        var attribute = this.element.getAttribute("data-tilt-" + property);
         try {
           newSettings[property] = JSON.parse(attribute);
         } catch (e) {
           newSettings[property] = attribute;
         }
-
       } else {
         newSettings[property] = defaultSettings[property];
       }
     }
 
     return newSettings;
-  }
+  };
 
-  static init(elements, settings) {
+  VanillaTilt.init = function init(elements, settings) {
     if (elements instanceof Node) {
       elements = [elements];
     }
@@ -188,13 +194,15 @@ class VanillaTilt {
       return;
     }
 
-    elements.forEach((element) => {
+    elements.forEach(function (element) {
       if (!("vanillaTilt" in element)) {
         element.vanillaTilt = new VanillaTilt(element, settings);
       }
     });
-  }
-}
+  };
+
+  return VanillaTilt;
+}();
 
 if (typeof document !== "undefined") {
   /* expose the class to window */
@@ -206,6 +214,4 @@ if (typeof document !== "undefined") {
   VanillaTilt.init(document.querySelectorAll("[data-tilt]"));
 }
 
-return VanillaTilt;
-
-}());
+module.exports = VanillaTilt;
